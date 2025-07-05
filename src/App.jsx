6bfd1b40629307e4056db7b8e9e111b0fa916a9c
frontendnitin <nitinchauhan707864@gmail.com>
2025-07-05 16:50:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Component/Navbar/navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component/Navbar/NavbarBanner/navbarBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./navbarBanner.css";
+const NavbarBanner = () => {
+  return <div>NavbarBanner</div>;
+};
+
+export default NavbarBanner;
